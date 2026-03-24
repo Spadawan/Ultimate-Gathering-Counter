e7a5fc147139361e20db66bb3b1806722ec15605
Spadawan <simon.paindavoine@gmail.com>
2026-03-24 08:04:06 -0400

    Fix leather category fallback to ignore equipment

--- Compat.lua
+++ Compat.lua
@@ -126,10 +126,11 @@ function Compat:GetItemCategoryFromInfo(itemID)
         if haystack:find("fish", 1, true) or haystack:find("poisson", 1, true) then
             return "fish"
         end
-        if haystack:find("leather", 1, true) or haystack:find("cuir", 1, true)
+        local isEquipment = (classID == LE_ITEM_CLASS_WEAPON) or (classID == LE_ITEM_CLASS_ARMOR)
+        if not isEquipment and (haystack:find("leather", 1, true) or haystack:find("cuir", 1, true)
             or haystack:find("hide", 1, true) or haystack:find("peau", 1, true)
             or haystack:find("scale", 1, true) or haystack:find("écaille", 1, true)
-            or haystack:find("bone", 1, true) or haystack:find("os", 1, true) then
+            or haystack:find("bone", 1, true) or haystack:find("os", 1, true)) then
             return "leather"
         end
     end
